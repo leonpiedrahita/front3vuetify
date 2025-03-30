8f15c544a8b2522318f8946ccf1efeb920d6d1f2
leonpiedrahita <leondpr@gmail.com>
2025-03-30 17:14:44 -0500

    Se cambia la rutrabdel store para que consulte en linea

--- src/store/index.js
+++ src/store/index.js
@@ -12,9 +12,9 @@ const store = createStore({
         ubicacion:'',
         icono:'',
         color:'',
-        ruta:'http://localhost:3001/',
+       /*  ruta:'http://localhost:3001/', */
         /*ruta:'https://backalternativo.1.us-1.fl0.io/',*/
-       /*  ruta:'https://backalternativo.onrender.com/', */
+        ruta:'https://backalternativo.onrender.com/', 
        identificacion:"",
        ordenes:{},
        equipo:{},

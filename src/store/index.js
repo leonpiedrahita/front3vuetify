4f53ae0301f96e3cd2735f1d1880380290f21af2
leonpiedrahita <leondpr@gmail.com>
2025-07-13 15:35:00 -0500

    cambiando ruta back

--- src/store/index.js
+++ src/store/index.js
@@ -12,9 +12,9 @@ const store = createStore({
         ubicacion:'',
         icono:'',
         color:'',
-        ruta:'http://localhost:5000/',
+       /*  ruta:'http://localhost:5000/', */
         /* ruta:'http://localhost:5000/', */
-       /*  ruta:'https://postgress-9isu.onrender.com/', */
+        ruta:'https://postgress-9isu.onrender.com/',
         /*ruta:'https://backalternativo.1.us-1.fl0.io/',*/
        /*  ruta:'https://backalternativo.onrender.com/',   */
        identificacion:"",

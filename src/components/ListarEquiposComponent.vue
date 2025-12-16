d96502b2875da22dd6890dada94c1dc23300a19b
leonpiedrahita <75052358+leonpiedrahita@users.noreply.github.com>
2025-12-16 10:16:41 -0500

    Update contract and state lists in ListarEquiposComponent

--- src/components/ListarEquiposComponent.vue
+++ src/components/ListarEquiposComponent.vue
@@ -568,8 +568,8 @@ export default {
     etapaautorizada: "Desinfección",
     observaciones: "",
     listadeetapas: [],
-    listacontratos: ["Sin asignar", "Comodato", "Venta", "Venta Externo", "Alquiler", "Préstamo", "Demostración", "Dado de Baja", "Devuelto al Proveedor"],
-    listaestados: ["Nuevo","En servicio", "Disponible", "Disponible Pdte. MP", "En Soporte"],
+    listacontratos: ["Sin asignar", "Comodato", "Venta", "Venta Externo", "Alquiler", "Préstamo", "Demostración", "Fuera de Servicio", "Devuelto al Proveedor"],
+    listaestados: ["Nuevo","En servicio", "Disponible", "Disponible Pdte. MP", "En Soporte", "Dado de Baja"],
     nombreUbicacionesCliente: [],
     nombreUbicacionesClienteModificado: [],
     buscar: {

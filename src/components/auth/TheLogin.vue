8f17c6f9b6d5600cb734e9cd1340f88158050f64
leonpiedrahita <leondpr@gmail.com>
2026-02-01 20:43:01 -0500

    quitando el aceptar del bienvenido

--- src/components/auth/TheLogin.vue
+++ src/components/auth/TheLogin.vue
@@ -27,23 +27,20 @@
             <span class="separator"></span>
             Todos los derechos reservados 2026.
             <span class="separator"></span>
-            V.1.4.1
+            V.1.4.2
           </div>
         </v-row>
       </v-col>
       <v-dialog transition="dialog-bottom-transition" max-width="600" persistent v-model="confirmacionlogin">
-        <v-card>
-          <v-toolbar class="text-h4" color="primary" dark>¡Bienvenid@!</v-toolbar>
-          <v-card-text>
-            <div class="text-h5 pa-3">
-              {{ this.nombre }}
-            </div>
-          </v-card-text>
-          <v-card-actions class="justify-center">
-            <v-btn class="c6" @click="LoginAceptado">Aceptar</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+  <v-card>
+    <v-toolbar class="text-h4" color="primary" dark>¡Bienvenid@!</v-toolbar>
+    <v-card-text>
+      <div class="text-h5 pa-3">
+        {{ this.nombre }}
+      </div>
+    </v-card-text>
+  </v-card>
+</v-dialog>
       <v-dialog transition="dialog-bottom-transition" max-width="600" persistent v-model="errorlogin">
         <v-card>
           <v-toolbar class="text-h4" color="error" dark>¡Oops! Algo salió mal</v-toolbar>
@@ -107,26 +104,27 @@ export default {
       }
       console.log("Cookies de sesión previa limpiadas.");
     },
-    async loginUser() {
+   async loginUser() {
+  axios
+    .post(this.$store.state.ruta + "api/usuario/ingresar", this.login)
+    .then((response) => {
+      return response.data;
+    })
+    .then((data) => {
+      this.$store.dispatch("guardarToken", data.tokenReturn);
+      this.nombre = this.$store.state.user.nombre;
+      this.confirmacionlogin = true;
 
-
-      axios
-        .post(this.$store.state.ruta + "api/usuario/ingresar", this.login)
-        .then((response) => {
-          return response.data;
-        })
-        .then((data) => {
-          this.$store.dispatch("guardarToken", data.tokenReturn);//se guarda el token en la tienda
-          this.nombre = this.$store.state.user.nombre,
-            this.confirmacionlogin = true
-/*             console.log(data);
- */          })
-        .catch((error) => {
-          this.errorlogin = true;
-          console.log(error);
-          return error;
-        });
-    },
+      // --- NUEVA LÓGICA AUTOMÁTICA ---
+      setTimeout(() => {
+        this.LoginAceptado();
+      }, 2000); // 3000 milisegundos = 3 segundos
+    })
+    .catch((error) => {
+      this.errorlogin = true;
+      console.log(error);
+    });
+},
     LoginAceptado() {
       this.confirmacionlogin = false
       this.$router.push({ name: 'ListarEquipos' });
